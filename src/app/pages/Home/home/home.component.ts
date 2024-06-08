4f1dd501644a7c641644cc1b2ba13884a5181089
Tfanios <fanis1337@gmail.com>
2024-06-08 23:42:08 +0300

    infinite scrol / get images as subject

--- src/app/pages/Home/home/home.component.ts
+++ src/app/pages/Home/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { ImagesService } from 'src/app/services/images/images.service';
 
 @Component({
@@ -10,15 +10,26 @@ export class HomeComponent implements OnInit {
   constructor(private imageSerivce : ImagesService){}
   images:string[] = []
   likedImages:string[] = []
+  isLoading = false
   ngOnInit() {
-    this.imageSerivce.getImages().subscribe(images=>this.images = images);
+    // this.imageSerivce.getImages().subscribe(images=>this.images = images);
     this.imageSerivce.getLikedImages().subscribe(images=>this.likedImages = images);
+    this.images = this.imageSerivce.getImagesWithSubject()
     if(localStorage?.getItem('userAndFavoriteImages')){
       const data =JSON.parse(localStorage.getItem('userAndFavoriteImages')!)
       this.likedImages = data.imagesId;
       console.log(JSON.parse(localStorage.getItem('userAndFavoriteImages')!))
     }
+  }
 
+  @HostListener('window:scroll',['$event'])
+  onWindowScroll(event:any){
+    console.log(this.likedImages)
+    if(window.innerHeight+window.scrollY>=document.body.querySelector<HTMLElement>('app-root')!.offsetHeight ){
+      // console.log(window.innerHeight+window.scrollY,'y+x');
+      console.log(event);
+      this.imageSerivce.getImagesByScroll();
+    }
   }
 }
 
--- src/app/services/images/images.service.ts
+++ src/app/services/images/images.service.ts
@@ -7,17 +7,18 @@ import { BehaviorSubject, Observable, map, of } from 'rxjs';
 export class ImagesService {
   constructor() { }
   private likedImagesSubject: BehaviorSubject<string[]> = new BehaviorSubject<string[]>([]);
-  likedImages$: Observable<string[]> = this.likedImagesSubject.asObservable();
+  private imagesSubject: BehaviorSubject<string[]> = new BehaviorSubject<string[]>([]);
 
+ 
+  likedImages$: Observable<string[]> = this.likedImagesSubject.asObservable();
+  numberOfImages = 50
   getImages() : Observable<string[]>{
-    const numberOfImages = 50
-    const images = [...Array(numberOfImages)].map((image, i) => `https://picsum.photos/id/${i}/300/300`)
-   
+    const images = [...Array(this.numberOfImages)].map((image, i) => `https://picsum.photos/id/${i}/300/300`)
     return of(images)
   }
 
+
   likeImage (imageId: string) {
-    console.log('liked', imageId)
     if (imageId) {
       const currentLikedImages = this.likedImagesSubject.getValue();
       if (!currentLikedImages.includes(imageId)) {
@@ -29,22 +30,45 @@ export class ImagesService {
     const currentLikedImages = this.likedImagesSubject.getValue();
     const updatedLikedImages = currentLikedImages.filter((id) => id !== imageId);
     this.likedImagesSubject.next(updatedLikedImages);
-   
+    console.log(currentLikedImages,'currentLikedImages')
+    console.log(updatedLikedImages,'updatedLikedImages')
+
     const data = JSON.parse(localStorage.getItem('userAndFavoriteImages')!)
     
     const updatedImagesId = data.imagesId.filter((img:string) =>{
         return img != imageId;
     })
-    console.log(data)
-    if(updatedImagesId > 1){
+   
+    if(updatedImagesId.length > 0){
       localStorage.setItem('userAndFavoriteImages', JSON.stringify({id:data.id, imagesId:updatedImagesId}))
     }else{
       localStorage.setItem('userAndFavoriteImages', JSON.stringify({id:data.id, imagesId:[]}))
     }
     
   }
+
   getLikedImages(): Observable<string[]> {
     return this.likedImages$;
+  }
+  getImagesByScroll() {
+    const lastImageIdx = this.numberOfImages
+    let newIndeces:string[] = []
+    for(let i = this.numberOfImages + 1; i <= this.numberOfImages+10; i++){
+      
+        newIndeces.push(`https://picsum.photos/id/${i}/300/300`)
+    }
+    console.log(newIndeces)
+    const currentImages = this.imagesSubject.getValue()
+    newIndeces.forEach((el)=>{
+      currentImages.push(el);
+    })
+    this.imagesSubject.next(currentImages);
+
+  }
+  getImagesWithSubject() :string[]{
+    const images = [...Array(this.numberOfImages)].map((image, i) => `https://picsum.photos/id/${i}/300/300`)
+    this.imagesSubject.next(images);
+    return images
   }
  
 }
